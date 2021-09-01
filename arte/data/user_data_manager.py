e888e0c1bd58257a15556d802190ea711737dac5
swogks <wogks3000@gmail.com>
2021-09-01 23:07:00 +0900

    [ADD] OrderRecorder
    오더 레코더가 추가되었습니다.

--- arte/data/user_data_manager.py
+++ arte/data/user_data_manager.py
@@ -7,11 +7,14 @@ from binance_f.model import *
 from binance_f.exception.binanceapiexception import BinanceApiException
 from binance_f.base.printobject import *
 
+from arte.system.order_recorder import OrderRecorder
+
 
 class UserDataManager:
-    def __init__(self, client, account) -> None:
+    def __init__(self, client, account, order_recorder) -> None:
         self.client = client
         self.account = account
+        self.order_recorder = order_recorder
         self.listen_key = None
 
     def open_user_data_socket(self):
@@ -23,29 +26,44 @@ class UserDataManager:
             elif data_type == SubscribeMessageType.PAYLOAD:
                 if event.eventType == "ACCOUNT_UPDATE":
                     self.account.update(event)
-                    print("USDT: ", self.account["USDT"])
-                    print("ETHUSDT: ", self.account["ETHUSDT"])
+                    # print("USDT: ", self.account["USDT"])
+                    # print("ETHUSDT: ", self.account["ETHUSDT"])
+                elif event.eventType == "ORDER_TRADE_UPDATE":
+                    self.order_recorder.get_event(event)
+                    print(self.order_recorder.pair_dict)
                     # print("Event Type: ", event.eventType)
                     # print("Event time: ", event.eventTime)
-                    # print("Transaction time: ", event.transactionTime)
-                    # print("=== Balances ===")
-                    # PrintMix.print_data(event.balances)
-                    # print("================")
-                    # print("=== Positions ===")
-                    # PrintMix.print_data(event.positions)
-                    # print("================")
-                elif event.eventType == "ORDER_TRADE_UPDATE":
-                    if event.orderStatus == "NEW":
-                        pass
-                        # self.order_handler.update_not_signed_order(event)  # update not signed order
-                    elif event.orderStatus == "FILLED":
-                        if event.side == "BUY":
-                            pass
-                            # self.order_handler.update_signed_order(event)  # update signed order
-                            # self.order_handler.delete_not_signed_order(event.clientOrderId)  # delete not signed order
-                        elif event.side == "SELL":
-                            pass
-                            # self.order_handler.delete_signed_order(event.clientOrderId)  # delete signed order
+                    # print("Transaction Time: ", event.transactionTime)
+                    # print("Symbol: ", event.symbol)
+                    # print("Client Order Id: ", event.clientOrderId)
+                    # print("Side: ", event.side)
+                    # print("Order Type: ", event.type)
+                    # print("Time in Force: ", event.timeInForce)
+                    # print("Original Quantity: ", event.origQty)
+                    # print("Position Side: ", event.positionSide)
+                    # print("Price: ", event.price)
+                    # print("Average Price: ", event.avgPrice)
+                    # print("Stop Price: ", event.stopPrice)
+                    # print("Execution Type: ", event.executionType)
+                    # print("Order Status: ", event.orderStatus)
+                    # print("Order Id: ", event.orderId)
+                    # print("Order Last Filled Quantity: ", event.lastFilledQty)
+                    # print("Order Filled Accumulated Quantity: ", event.cumulativeFilledQty)
+                    # print("Last Filled Price: ", event.lastFilledPrice)
+                    # print("Commission Asset: ", event.commissionAsset)
+                    # print("Commissions: ", event.commissionAmount)
+                    # print("Order Trade Time: ", event.orderTradeTime)
+                    # print("Trade Id: ", event.tradeID)
+                    # print("Bids Notional: ", event.bidsNotional)
+                    # print("Ask Notional: ", event.asksNotional)
+                    # print("Is this trade the maker side?: ", event.isMarkerSide)
+                    # print("Is this reduce only: ", event.isReduceOnly)
+                    # print("stop price working type: ", event.workingType)
+                    # print("Is this Close-All: ", event.isClosePosition)
+                    # if not event.activationPrice is None:
+                    #     print("Activation Price for Trailing Stop: ", event.activationPrice)
+                    # if not event.callbackRate is None:
+                    #     print("Callback Rate for Trailing Stop: ", event.callbackRate)
             elif event.eventType == "listenKeyExpired":
                 print("listenKey Expired, Reconnect Socket again.")
                 self.open_user_data_socket()
@@ -70,7 +88,8 @@ if __name__ == "__main__":
 
     cli = Client("TEST")
     acc = Account(cli.request_client)
-    udm = UserDataManager(cli, acc)
+    order_rec = OrderRecorder()
+    udm = UserDataManager(cli, acc, order_rec)
     udm.open_user_data_socket()
 
     for t in threading.enumerate():
---
+++ arte/system/order_recorder.py
@@ -0,0 +1,17 @@
+"""
+오더를 레코드 합니다.
+"""
+from collections import namedtuple
+
+
+class OrderRecorder:
+    def __init__(self):
+        self.pair_dict = {}
+
+    def get_event(self, event):
+        print(event.clientOrderId)
+        if event.orderStatus == "NEW":
+            self.pair_dict[event.clientOrderId] = {"beforeFill": event, "afterFill": None}
+        elif event.orderStatus == "FILLED":
+            self.pair_dict[event.clientOrderId]["afterFill"] = event
+
