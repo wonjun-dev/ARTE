15a71c0b7657fb656b76b90403a03fc01577b2e5
swogks <wogks3000@gmail.com>
2021-09-29 22:41:15 +0900

    [ADD] backtest system initial version
    - 백테스트 시스템용 트레이드 매니저가 추가되었습니다.
    - 기존 사용과 같은 방식으로 사용되며 market 거래만 가능합니다.

---
+++ arte/test_system/test_order_handler.py
@@ -0,0 +1,179 @@
+from decimal import Decimal, getcontext
+
+getcontext().prec = 6
+
+from functools import wraps
+
+import numpy as np
+
+from binance_f.model import Order
+from binance_f.model.constant import *
+
+from arte.test_system.test_account import TestAccount
+
+MAKER_FEE_RATE = 0.0002
+TAKER_FEE_RATE = 0.0004
+
+
+def _print_status(method):
+    @wraps(method)
+    def _impl(self, *args, **kwargs):
+        method(self, *args, **kwargs)
+        print(self.account)
+
+    return _impl
+
+
+class TestOrderHandler:
+    def __init__(self, account):
+        self.account = account
+        self.short_tracker = dict()
+
+    def _init_short_tracker(self):
+        return {"avgPrice": Decimal(0), "quantity": Decimal(0)}
+
+    def _select_fee_rate(self):
+        return Decimal(TAKER_FEE_RATE)
+
+    def _check_ratio_range(self, ratio):
+        return True if 0 < ratio <= 1 else False
+
+    @_print_status
+    def open_long_market(self, symbol, price, ratio=None, usdt=None):
+        fee_rate = self._select_fee_rate()
+        price = Decimal(price)
+        if ratio:
+            if self._check_ratio_range(ratio):
+                usdt_to_buy = Decimal(self.account["USDT"]) * Decimal(ratio)
+            else:
+                print("Warning: ratio should be in range (0,1]")
+                return None
+        elif usdt:
+            usdt_to_buy = Decimal(usdt)
+        fee = usdt_to_buy * fee_rate
+        whole_cost = usdt_to_buy + fee
+        quantity = usdt_to_buy / price
+        if self.account["USDT"] > whole_cost:
+            o = Order()
+            o.symbol = symbol
+            o.origQty = float(quantity)
+            o.avgPrice = float(price)
+            o.side = OrderSide.BUY
+            o.positionSide = PositionSide.LONG
+            o.type = OrderType.MARKET
+            o.status = "FILLED"
+
+            self.account.withdraw_usdt(whole_cost)
+            self.account.deposit(symbol, PositionSide.LONG, quantity)
+            return o
+        else:
+            return None
+
+    @_print_status
+    def close_long_market(self, symbol, price, ratio):
+        fee_rate = self._select_fee_rate()
+        if not self._check_ratio_range(ratio):
+            print("Warning: ratio should be in range (0,1]")
+            return None
+        price = Decimal(price)
+        ratio = Decimal(ratio)
+        if not (self.account[symbol][PositionSide.LONG] > 0):
+            print("You cannot sell, no position to sell")
+            return None
+        quantity = Decimal(self.account[symbol][PositionSide.LONG]) * ratio
+        usdt_by_sell = quantity * price
+        fee = usdt_by_sell * fee_rate
+        whole_earn = usdt_by_sell - fee
+        o = Order()
+        o.symbol = symbol
+        o.origQty = float(quantity)
+        o.avgPrice = float(price)
+        o.side = OrderSide.SELL
+        o.positionSide = PositionSide.LONG
+        o.type = OrderType.MARKET
+        o.status = "FILLED"
+
+        self.account.deposit_usdt(whole_earn)
+        self.account.withdraw(symbol, PositionSide.LONG, quantity)
+        return o
+
+    @_print_status
+    def open_short_market(self, symbol, price, ratio=None, usdt=None):
+        fee_rate = self._select_fee_rate()
+        price = Decimal(price)
+        if ratio:
+            if self._check_ratio_range(ratio):
+                usdt_to_buy = Decimal(self.account["USDT"]) * Decimal(ratio)
+            else:
+                print("ratio should be in range (0,1]")
+                return None
+        elif usdt:
+            usdt_to_buy = Decimal(usdt)
+        fee = usdt_to_buy * fee_rate
+        whole_cost = usdt_to_buy + fee
+        quantity = usdt_to_buy / price
+        if self.account["USDT"] > whole_cost:
+            o = Order()
+            o.symbol = symbol
+            o.origQty = float(quantity)
+            o.avgPrice = float(price)
+            o.side = OrderSide.SELL
+            o.positionSide = PositionSide.SHORT
+            o.type = OrderType.MARKET
+            o.status = "FILLED"
+
+            self.account.withdraw_usdt(whole_cost)
+            self.account.deposit(symbol, PositionSide.SHORT, quantity)
+
+            if symbol not in self.short_tracker.keys():
+                self.short_tracker[symbol] = self._init_short_tracker()
+
+            self.short_tracker[symbol]["avgPrice"] = (
+                self.short_tracker[symbol]["avgPrice"] * self.short_tracker[symbol]["quantity"] + usdt_to_buy
+            ) / (self.short_tracker[symbol]["quantity"] + quantity)
+            self.short_tracker[symbol]["quantity"] += quantity
+            return o
+        else:
+            return None
+
+    @_print_status
+    def close_short_market(self, symbol, price, ratio):
+        fee_rate = self._select_fee_rate()
+        if not self._check_ratio_range(ratio):
+            print("Warning: ratio should be in range (0,1]")
+            return None
+        price = Decimal(price)
+        ratio = Decimal(ratio)
+        if not (self.account[symbol][PositionSide.SHORT] > 0):
+            print("You cannot sell, no position to sell")
+            return None
+        quantity = self.account[symbol][PositionSide.SHORT] * ratio
+        sell_price = self.short_tracker[symbol]["avgPrice"] * 2 - price
+        usdt_by_sell = quantity * sell_price
+        fee = usdt_by_sell * fee_rate
+        whole_earn = usdt_by_sell - fee
+        o = Order()
+        o.symbol = symbol
+        o.origQty = float(quantity)
+        o.avgPrice = float(price)
+        o.side = OrderSide.BUY
+        o.positionSide = PositionSide.SHORT
+        o.type = OrderType.MARKET
+        o.status = "FILLED"
+
+        self.account.deposit_usdt(whole_earn)
+        self.account.withdraw(symbol, PositionSide.SHORT, quantity)
+
+        # short_tracker 반영
+        self.short_tracker[symbol]["quantity"] -= quantity
+        return o
+
+
+if __name__ == "__main__":
+    acc = TestAccount(init_balance=1000)
+    oh = TestOrderHandler(acc)
+    oh.open_long_market(symbol="BTCUSDT", price=200, usdt=100)
+    oh.close_long_market(symbol="BTCUSDT", price=300, ratio=1)
+    oh.open_short_market(symbol="BTCUSDT", price=200, usdt=100)
+    oh.close_short_market(symbol="BTCUSDT", price=100, ratio=1)
+    # print(acc)
---
+++ arte/test_system/test_trade_manager.py
@@ -0,0 +1,102 @@
+from functools import wraps
+from decimal import Decimal, getcontext
+
+getcontext().prec = 6
+
+from binance_f.model.constant import *
+from arte.test_system.test_account import TestAccount
+from arte.test_system.test_order_handler import TestOrderHandler
+from arte.system.order_recorder import OrderRecorder
+
+
+def _process_order(method):
+    @wraps(method)
+    def _impl(self, *args, **kwargs):
+        args = list(args)
+        args[0] = args[0].upper()
+        if args[0] not in self.symbols_state:
+            self.symbols_state[args[0]] = self._init_symbol_state()
+        order = method(self, *args, **kwargs)
+        if order:
+            self._postprocess_order(order)
+        return order
+
+    return _impl
+
+
+class TestTradeManager:
+    def __init__(self, init_usdt=5000, *args, **kwargs):
+        self.account = TestAccount(init_balance=init_usdt)
+        self.order_handler = TestOrderHandler(self.account)
+        self.order_recorder = OrderRecorder()
+
+        self.bot = None
+        if "bot" in kwargs:
+            self.bot = kwargs["bot"]
+        if "max_order_count" in kwargs:
+            self.max_order_count = kwargs["max_order_count"]
+
+        # state manage
+        self.symbols_state = dict()
+
+    def _init_symbol_state(self):
+        return dict(order_count=0, positionSize=0, positionSide=PositionSide.INVALID)
+
+    @_process_order
+    def buy_long_market(self, symbol, price, usdt=None, ratio=None):
+        if self.symbols_state[symbol]["order_count"] < self.max_order_count:
+            return self.order_handler.open_long_market(symbol=symbol, price=price, usdt=usdt, ratio=ratio)
+
+    @_process_order
+    def buy_short_market(self, symbol, price, usdt=None, ratio=None):
+        if self.symbols_state[symbol]["order_count"] < self.max_order_count:
+            return self.order_handler.open_short_market(symbol=symbol, price=price, usdt=usdt, ratio=ratio)
+
+    @_process_order
+    def sell_long_market(self, symbol, price, ratio):
+        return self.order_handler.close_long_market(symbol=symbol, price=price, ratio=ratio)
+
+    @_process_order
+    def sell_short_market(self, symbol, price, ratio):
+        return self.order_handler.close_short_market(symbol=symbol, price=price, ratio=ratio)
+
+    def _postprocess_order(self, order):
+        symbol = order.symbol
+        if self._is_buy_or_sell(order) == "BUY":
+            self.symbols_state[symbol]["order_count"] += 1
+            self.symbols_state[symbol]["positionSize"] = float(
+                Decimal(self.symbols_state[symbol]["positionSize"] + order.origQty)
+            )
+            self.symbols_state[symbol]["positionSide"] = order.positionSide
+
+        elif self._is_buy_or_sell(order) == "SELL":
+            self.symbols_state[symbol]["positionSize"] = float(
+                Decimal(self.symbols_state[symbol]["positionSize"] - order.origQty)
+            )
+            if self.symbols_state[symbol]["positionSize"] == 0:
+                self.symbols_state[symbol] = self._init_symbol_state()
+
+        # message = f"Order {order.clientOrderId}: {order.side} {order.positionSide} {order.type} - {order.symbol} / Qty: {order.origQty}, Price: ${order.avgPrice}"
+
+    @staticmethod
+    def _is_buy_or_sell(order):
+        if ((order.side == OrderSide.BUY) & (order.positionSide == PositionSide.LONG)) or (
+            (order.side == OrderSide.SELL) & (order.positionSide == PositionSide.SHORT)
+        ):
+            return "BUY"
+        elif ((order.side == OrderSide.SELL) & (order.positionSide == PositionSide.LONG)) or (
+            (order.side == OrderSide.BUY) & (order.positionSide == PositionSide.SHORT)
+        ):
+            return "SELL"
+        else:
+            raise ValueError("Cannot check order is buy or sell")
+
+
+if __name__ == "__main__":
+
+    tm = TestTradeManager(init_usdt=1000, max_order_count=3)
+    tm.buy_long_market("ethusdt", price=2783, usdt=100)
+    tm.sell_long_market("ethusdt", price=2700, ratio=0.5)
+    # tm.buy_short_market("ethusdt", price=2783, usdt=100)
+    # tm.sell_short_market("ethusdt", price=2700, ratio=1)
+
