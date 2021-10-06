6fed8c9f21416a7039f2138e63cfe3db5aeb75c6
Gyu00 <david12070@gmail.com>
2021-10-06 23:49:38 +0900

    Update test_data_loader.py
    2. 메모리 최적화 :  Trade 를  concat 해서 합치는게 아니고, 하루치 ohlcv를 싹 계산 후에 ohlcv를 concat하는게 메모리적으로 이득일듯
    Upbit 완료
    Binance 완료
    5. frequency 조정 가능하게

--- arte/test_system/test_data_loader.py
+++ arte/test_system/test_data_loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from tqdm import tqdm
 from datetime import datetime, timedelta
 
 from arte.data.trade_parser import TradeParser
@@ -15,15 +16,13 @@ class TestDataLoader:
         self.upbit_ohlcv_list = dict()
         self.binance_ohlcv_list = dict()
 
-    def init_test_data_loader(self, symbols, start_date, end_date, freq="250ms"):
+    def init_test_data_loader(self, symbols, start_date, end_date, ohlcv_interval=250):
 
         self.symbols = [symbol.upper() for symbol in symbols]
         self.start_date = datetime.strptime(start_date, "%Y-%m-%d")
         self.end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        self.freq = freq
-
-        # self.upbit_symbols = ["KRW-" + symbol for symbol in symbols]
-        # self.binance_symbols = [symbol + "USDT" for symbol in symbols]
+        self.freq = str(ohlcv_interval) + "ms"
+        self.ohlcv_interval = ohlcv_interval
 
         self.upbit_trade = TradeParser(symbols=self.symbols)
         self.binance_trade = TradeParser(symbols=self.symbols)
@@ -35,111 +34,122 @@ class TestDataLoader:
         self.end_current_time = pd.to_datetime(end_date) + timedelta(days=1)
         self.count = 0
 
-        for symbol in self.symbols:
+        for symbol in tqdm(self.symbols):
             self.upbit_ohlcv_list[symbol] = self.upbit_ohlcv[symbol].to_dict("records")
             self.binance_ohlcv_list[symbol] = self.binance_ohlcv[symbol].to_dict("records")
 
         print("Complete Data Loading")
 
     def init_upbit_test_loader(self):
-        for symbol in self.symbols:
-            trade_df = self.load_trade_data(symbol, is_upbit=True)
-            self.upbit_convert_to_ohlcv(symbol, trade_df)
 
-    def upbit_convert_to_ohlcv(self, symbol, upbit_trade_df):
+        for symbol in tqdm(self.symbols):
+            ohlcv_list = []
+
+            current_date = self.start_date
+            while current_date <= self.end_date:
+                trade_df = self.load_trade_data(symbol, current_date, is_upbit=True)
+                oneday_ohlcv = self.upbit_convert_to_ohlcv(trade_df, current_date)
+                ohlcv_list.append(oneday_ohlcv)
+                current_date += timedelta(days=1)
+
+            self.upbit_ohlcv[symbol] = pd.concat(ohlcv_list)
+            self.upbit_ohlcv[symbol].reset_index(drop=True, inplace=True)
+
+    def upbit_convert_to_ohlcv(self, upbit_trade_df, current_date):
         gp = make_group(upbit_trade_df, freq=self.freq)
-        self.upbit_ohlcv[symbol] = gp["price"].ohlc()
-        self.upbit_ohlcv[symbol]["volume"] = gp["quantity"].sum()
-        self.upbit_ohlcv[symbol]["trade_num"] = gp["trade_num"].sum()
+        temp_upbit_ohlcv = gp["price"].ohlc()
+        temp_upbit_ohlcv["volume"] = gp["quantity"].sum()
+        temp_upbit_ohlcv["trade_num"] = gp["trade_num"].sum()
 
-        self.upbit_ohlcv[symbol].fillna(
-            dict.fromkeys(self.upbit_ohlcv[symbol].columns.tolist(), self.upbit_ohlcv[symbol].close.ffill()),
+        temp_upbit_ohlcv.fillna(
+            dict.fromkeys(temp_upbit_ohlcv.columns.tolist(), temp_upbit_ohlcv.close.ffill()),
             inplace=True,
         )
 
-        start_stamp = pd.to_datetime(self.start_date)
-        end_stamp = self.upbit_ohlcv[symbol].index[0]
-        temp = self.upbit_ohlcv[symbol].iloc[0]["open"]
+        start_stamp = pd.to_datetime(current_date)
+        end_stamp = temp_upbit_ohlcv.index[0]
+        temp = temp_upbit_ohlcv.iloc[0]["open"]
 
         while start_stamp < end_stamp:
-            self.upbit_ohlcv[symbol].loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
-            start_stamp += timedelta(milliseconds=250)
+            temp_upbit_ohlcv.loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
+            start_stamp += timedelta(milliseconds=self.ohlcv_interval)
 
-        self.upbit_ohlcv[symbol].sort_index(inplace=True)
+        temp_upbit_ohlcv.sort_index(inplace=True)
 
-        start_stamp = self.upbit_ohlcv[symbol].index[-1] + timedelta(milliseconds=250)
-        end_stamp = pd.to_datetime(self.end_date) + timedelta(days=1)
-        temp = self.upbit_ohlcv[symbol].iloc[-1]["close"]
+        start_stamp = temp_upbit_ohlcv.index[-1] + timedelta(milliseconds=self.ohlcv_interval)
+        end_stamp = pd.to_datetime(current_date) + timedelta(days=1)
+        temp = temp_upbit_ohlcv.iloc[-1]["close"]
 
         while start_stamp < end_stamp:
-            self.upbit_ohlcv[symbol].loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
-            start_stamp += timedelta(milliseconds=250)
+            temp_upbit_ohlcv.loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
+            start_stamp += timedelta(milliseconds=self.ohlcv_interval)
+
+        return temp_upbit_ohlcv
 
     def init_binance_test_loader(self):
-        for symbol in self.symbols:
-            trade_df = self.load_trade_data(symbol, is_upbit=False)
-            self.binance_conver_to_ohlcv(symbol, trade_df)
+        for symbol in tqdm(self.symbols):
+            ohlcv_list = []
 
-    def binance_conver_to_ohlcv(self, symbol, binance_trade_df):
+            current_date = self.start_date
+            while current_date <= self.end_date:
+                trade_df = self.load_trade_data(symbol, current_date, is_upbit=False)
+                oneday_ohlcv = self.binance_convert_to_ohlcv(trade_df, current_date)
+                ohlcv_list.append(oneday_ohlcv)
+                current_date += timedelta(days=1)
+
+            self.binance_ohlcv[symbol] = pd.concat(ohlcv_list)
+            self.binance_ohlcv[symbol].reset_index(drop=True, inplace=True)
+
+    def binance_convert_to_ohlcv(self, binance_trade_df, current_date):
         gp = make_group(binance_trade_df, freq=self.freq)
-        self.binance_ohlcv[symbol] = gp["price"].ohlc()
-        self.binance_ohlcv[symbol]["volume"] = gp["quantity"].sum()
-        self.binance_ohlcv[symbol]["trade_num"] = gp["trade_num"].sum()
+        temp_binance_ohlcv = gp["price"].ohlc()
+        temp_binance_ohlcv["volume"] = gp["quantity"].sum()
+        temp_binance_ohlcv["trade_num"] = gp["trade_num"].sum()
 
-        self.binance_ohlcv[symbol].fillna(
-            dict.fromkeys(self.binance_ohlcv[symbol].columns.tolist(), self.binance_ohlcv[symbol].close.ffill()),
+        temp_binance_ohlcv.fillna(
+            dict.fromkeys(temp_binance_ohlcv.columns.tolist(), temp_binance_ohlcv.close.ffill()),
             inplace=True,
         )
 
-        start_stamp = pd.to_datetime(self.start_date)
-        end_stamp = self.binance_ohlcv[symbol].index[0]
-        temp = self.binance_ohlcv[symbol].iloc[0]["open"]
+        start_stamp = pd.to_datetime(current_date)
+        end_stamp = temp_binance_ohlcv.index[0]
+        temp = temp_binance_ohlcv.iloc[0]["open"]
 
         while start_stamp < end_stamp:
-            self.binance_ohlcv[symbol].loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
-            start_stamp += timedelta(milliseconds=250)
+            temp_binance_ohlcv.loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
+            start_stamp += timedelta(milliseconds=self.ohlcv_interval)
 
-        self.binance_ohlcv[symbol].sort_index(inplace=True)
+        temp_binance_ohlcv.sort_index(inplace=True)
 
-        start_stamp = self.binance_ohlcv[symbol].index[-1] + timedelta(milliseconds=250)
-        end_stamp = pd.to_datetime(self.end_date) + timedelta(days=1)
-        temp = self.binance_ohlcv[symbol].iloc[-1]["close"]
+        start_stamp = temp_binance_ohlcv.index[-1] + timedelta(milliseconds=self.ohlcv_interval)
+        end_stamp = pd.to_datetime(current_date) + timedelta(days=1)
+        temp = temp_binance_ohlcv.iloc[-1]["close"]
 
         while start_stamp < end_stamp:
-            self.binance_ohlcv[symbol].loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
-            start_stamp += timedelta(milliseconds=250)
+            temp_binance_ohlcv.loc[start_stamp] = [temp, temp, temp, temp, 0.0, 0]
+            start_stamp += timedelta(milliseconds=self.ohlcv_interval)
 
-    def load_trade_data(self, symbol, is_upbit):
+        return temp_binance_ohlcv
+
+    def load_trade_data(self, symbol, current_date, is_upbit):
 
         if is_upbit:
             market_path = "upbit"
         else:
             market_path = "binance_spot"
 
-        trade_dataframe_list = []
-        current_date = self.start_date
+        file_path = os.path.join(
+            self.root_data_path,
+            market_path,
+            symbol,
+            f"{symbol}-"
+            + "{0:04d}-{1:02d}-{2:02d}.csv".format(current_date.year, current_date.month, current_date.day),
+        )
+        try:
+            output_df = pd.read_csv(file_path)
 
-        while current_date <= self.end_date:
-            file_path = os.path.join(
-                self.root_data_path,
-                market_path,
-                symbol,
-                f"{symbol}-"
-                + "{0:04d}-{1:02d}-{2:02d}.csv".format(current_date.year, current_date.month, current_date.day),
-            )
-
-            try:
-                current_df = pd.read_csv(file_path)
-
-            except:
-                print(f"Error : Do not exist {file_path}!\n")
-                break
-
-            trade_dataframe_list.append(current_df)
-            current_date += timedelta(days=1)
-
-        output_df = pd.concat(trade_dataframe_list)
-        output_df.reset_index(drop=True, inplace=True)
+        except:
+            print(f"Error : Do not exist {file_path}!\n")
 
         output_df["trade_num"] = [1] * len(output_df)
 
@@ -154,14 +164,14 @@ class TestDataLoader:
                 self.binance_trade.price[symbol] = self.binance_ohlcv_list[symbol][self.count]["close"]
 
             self.count += 1
-            self.current_time += timedelta(milliseconds=250)
+            self.current_time += timedelta(milliseconds=self.ohlcv_interval)
             return True
         else:
             return False
 
     def get_counter(self):
         td = self.end_current_time - self.current_time
-        counter = td / timedelta(milliseconds=250)
+        counter = td / timedelta(milliseconds=self.ohlcv_interval)
         return int(counter)
 
     def load_next_by_counter(self, counter):
