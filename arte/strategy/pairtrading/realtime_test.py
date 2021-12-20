f3311f28d2736690670ddf6c6789faac8afc3513
Gyu00 <64250761+Gyu00@users.noreply.github.com>
2021-12-20 20:33:15 +0900

    update

---
+++ arte/strategy/pairtrading/realtime_test.py
@@ -0,0 +1,96 @@
+import traceback
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pandas import Timestamp
+
+from arte.data import SocketDataManager
+from arte.data import RequestDataManager
+from arte.data.common_symbol_collector import CommonSymbolCollector
+from arte.test_system import RTTUpbitTradeManager
+
+from strategy_loop import StrategyLoop
+
+
+class TradeScheduler:
+    def __init__(self, client, **kwargs):
+        self.client = client
+        self.scheduler = BlockingScheduler()
+        self.socket_data_manager = SocketDataManager(self.client)
+        self.request_data_manager = RequestDataManager(self.client)
+        # self.symbol_collector = CommonSymbolCollector()
+
+        # Init bot
+        self.bot = None
+        if "bot" in kwargs:
+            self.bot = kwargs["bot"]
+            self.bot.trader = self
+
+        self.backtest_id = None
+        if "backtest_id" in kwargs:
+            self.backtest_id = kwargs["backtest_id"]
+
+        self.tm = RTTUpbitTradeManager(init_krw=400000, max_order_count=3, backtest_id=self.backtest_id)
+        self.strategy = StrategyLoop(trade_manager=self.tm)
+
+        # Init required data
+        self.except_list = []  # self.request_data_manager.get_closed_symbols()
+        self.exchange_rate = self.request_data_manager.get_usdt_to_kor()
+        # self.common_symbols = self.symbol_collector.get_future_symbol()
+        selected_assets = "BTC ETH BCH AAVE SOL LTC AXS ETC NEO DOT ATOM LINK QTUM OMG KAVA MANA EOS 1INCH ADA"
+        self.common_symbols = selected_assets.split(" ")
+
+    def mainloop(self):
+        try:
+            _cur_time = Timestamp.now()
+            self.tm.update(
+                test_current_time=_cur_time, orderbook=self.socket_data_manager.upbit_orderbook,
+            )
+            self.strategy.update(
+                upbit_price=self.socket_data_manager.upbit_trade,
+                binance_spot_price=self.socket_data_manager.binance_spot_trade,
+                exchange_rate=self.exchange_rate,
+                except_list=self.except_list,
+                current_time=_cur_time,
+            )
+            self.strategy.print_state()
+            self.strategy.run()
+
+        except Exception:
+            traceback.print_exc()
+
+    def start(self, watch_interval: float = 1.0):
+        self.socket_data_manager.open_upbit_orderbook_socket(symbols=self.common_symbols)
+        self.socket_data_manager.open_upbit_trade_socket(symbols=self.common_symbols)
+        self.socket_data_manager.open_binanace_spot_trade_socket(symbols=self.common_symbols)
+
+        self.strategy.initialize(self.common_symbols, self.except_list)
+
+        self.scheduler.add_job(self.mainloop, "interval", seconds=watch_interval)
+        self.scheduler.add_job(self.get_exchange_rate, "cron", minute="0")
+        # self.scheduler.add_job(self.update_closed_list, "cron", minute="0", second="1")
+        if self.bot:
+            self.bot.bot_manager_setting()
+        self.scheduler.start()
+
+    def get_exchange_rate(self):
+        self.exchange_rate = self.request_data_manager.get_usdt_to_kor()
+
+    def update_closed_list(self):
+        self.except_list = self.request_data_manager.get_closed_symbols()
+
+
+if __name__ == "__main__":
+    import configparser
+    from arte.system.client import Client
+
+    cfg = configparser.ConfigParser()
+    cfg.read("test/config.ini")
+    config = cfg["REAL"]
+
+    mode = config["MODE"]
+    api_key = config["API_KEY"]
+    secret_key = config["SECRET_KEY"]
+    use_bot = config.getboolean("USE_BOT")
+
+    clients = Client(mode, api_key, secret_key)
+    trader = TradeScheduler(clients, backtest_id="upbitfollow_20211128_rbt03")
+    trader.start()
---
+++ arte/strategy/pairtrading/signal_state.py
@@ -0,0 +1,120 @@
+import numpy as np
+from transitions import Machine
+from collections import deque
+
+from arte.system.utils import Timer
+from arte.system.utils import symbolize_upbit
+from arte.system.utils import symbolize_binance
+
+
+class SignalState:
+
+    states = ["idle", "buy_state", "buy_order_state", "sell_state", "sell_order_state"]
+
+    def __init__(self, symbol, tm_upbit, tm_binance):
+        self.symbol = symbol
+        self.tm_upbit = tm_upbit
+        self.tm_binance = tm_binance
+#작아지면 업ㅣ트 롱  바이난스 숏
+        transitions = [
+            {"trigger": "proceed", "source": "idle", "dest": "sell_state", "conditions": "have_open_position"},
+            {"trigger": "proceed", "source": "idle", "dest": "buy_state"},
+            {
+                "trigger": "proceed",
+                "source": "buy_state",
+                "dest": "buy_order_state",
+                "conditions": ["zscore_down"], 
+                # "conditions": ["premium_undershoot_mean"],
+                "after": "buy_long",
+            },
+            {
+                "trigger": "proceed",
+                "source": "sell_state",
+                "dest": "sell_order_state",
+                "conditions": ["zscore_comeback"],
+                "after": "sell_long",
+            },
+            {"trigger": "initialize", "source": "*", "dest": "idle"},  # , "before": "print_end"},
+        ]
+        m = Machine(
+            model=self,
+            states=SignalState.states,
+            transitions=transitions,
+            initial="idle",
+            after_state_change="auto_proceed",
+        )
+
+        self.is_open = False
+        self.premium_at_buy = None
+        self.price_at_buy = None
+        self.timer = Timer()
+
+    def print_end(self, **kwargs):
+        print(f"From {self.state} go back to Idle state.")
+
+    def auto_proceed(self, **kwargs):
+        if not self.state == "idle":
+            if not self.proceed(**kwargs):
+                self.initialize()
+
+    def have_open_position(self, **kwargs):
+        return self.is_open
+
+    # Buy logic and ordering
+    # def premium_over_threshold(self, **kwargs):
+    #     premium = kwargs["premium_q"][-1]
+    #     criteria_premium = kwargs["criteria_premium"]
+    #     return premium > criteria_premium * 1.2
+
+    def halflife_valid(self, **kwargs):
+        print(kwargs["half_life"])
+        return kwargs["half_life"] > 180
+
+    def zscore_down(self, **kwargs):
+        spread = list(kwargs["spread"])
+        #halflife = int(kwargs["half_life"])
+        #spread = spread[-halflife:]
+        zscore = (spread[-1]-np.mean(spread))/np.std(spread)
+        return zscore < -4
+
+    def zscore_comeback(self, **kwargs):
+        
+        spread = list(kwargs["spread"])
+        #halflife = int(kwargs["half_life"])
+        #spread = spread[-halflife:]
+        zscore = (spread[-1]-np.mean(spread))/np.std(spread)
+        return zscore >= 0
+        
+    def premium_undershoot_mean(self, **kwargs):
+        premium_q = list(kwargs["premium_q"])
+        change_rate = premium_q[-1] / (np.mean(premium_q[:9]))
+        return change_rate < 0.7
+
+    def binance_price_up(self, **kwargs):
+        binance_price_q = kwargs["binance_price_q"]
+        change_rate = binance_price_q[-1] / binance_price_q[0]
+        # change_rate = binance_price_q[-1] / np.mean([binance_price_q[i] for i in range(3, 8)])
+        return change_rate > 1.005
+
+    def upbit_price_stay(self, **kwargs):
+        price_q = kwargs["price_q"]
+        change_rate = price_q[-1] / price_q[0]
+        # change_rate = price_q[-1] / np.mean([price_q[i] for i in range(4, 9)])
+        return change_rate < 1.001
+
+    def buy_long(self, **kwargs):
+        self.initialize()
+        # print("Passed all signals, Order Buy long")
+        if self.tm_upbit.buy_long_market(symbol=symbolize_upbit(self.symbol), krw=100000):
+            self.is_open = True
+
+        self.tm_binance.buy_short_market(symbol=symbolize_binance(self.symbol), usdt=100)
+
+    def sell_long(self, **kwargs):
+        self.initialize()
+        # print("Passed all signals, Order Sell long")
+        if self.tm_upbit.sell_long_market(symbol=symbolize_upbit(self.symbol), ratio=1):
+            # print(f'{kwargs["premium_q"][-1]}')
+            self.is_open = False
+
+        self.tm_binance.sell_short_market(symbol=symbolize_binance(self.symbol), ratio=1)
