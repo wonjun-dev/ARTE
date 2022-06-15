4db90533ce06e3406445ab19d9f916404bfa9972
Gyu00 <64250761+Gyu00@users.noreply.github.com>
2022-06-16 01:15:44 +0900

    Update order_handler.py

--- arte/system/order_handler.py
+++ arte/system/order_handler.py
@@ -132,8 +132,8 @@ if __name__ == "__main__":
 
     cl = Client(
         mode="TEST",
-        api_key="0dcd28f57648b0a7d5ea2737487e3b3093d47935e67506b78291042d1dd2f9ea",
-        secret_key="b36dc15c333bd5950addaf92a0f9dc96d8ed59ea6835386c59a6e63e1ae26aa1",
+        api_key="",
+        secret_key="",
         req_only=True,
     )
     oh = OrderHandler(cl.request_client, Account(cl.request_client), "ETHUSDT")
