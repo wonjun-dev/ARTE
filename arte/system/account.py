622de62de43872d8f00ff20f515ba698cdaed286
Gyu00 <64250761+Gyu00@users.noreply.github.com>
2022-06-16 01:17:26 +0900

    Update account.py

--- arte/system/account.py
+++ arte/system/account.py
@@ -52,8 +52,8 @@ class Account:
 
 
 if __name__ == "__main__":
-    KEY = "0dcd28f57648b0a7d5ea2737487e3b3093d47935e67506b78291042d1dd2f9ea"
-    SECRET = "b36dc15c333bd5950addaf92a0f9dc96d8ed59ea6835386c59a6e63e1ae26aa1"
+    KEY = ""
+    SECRET = ""
     # BASE_URL = "https://fapi.binance.com"  # production base url
     BASE_URL = "https://testnet.binancefuture.com"  # testnet base url
     request_client = RequestClient(api_key=KEY, secret_key=SECRET, url=BASE_URL)
